4695e00bd2cb41f47c523cac0508c8f8b6ce3f47
SHArdow <>
2021-07-12 21:30:03 +1000

    upload webpage

---
+++ forwarder.js
@@ -0,0 +1,56 @@
+async function forwarder()
+{
+
+    document.getElementById("tokenSubmit").addEventListener
+    (
+
+        "mousedown",
+        (
+
+            async function()
+            {
+                var token = (document.getElementById("tokenInput").value);
+                var file = ("https://gist.githubusercontent.com/SHArdowYT" + "/" + token + "/" + "raw");
+
+                var request = await fetch(file);
+                var responseStatus = await (request.status);
+                var responseData = await (request.text());
+
+                if (responseStatus == 200)
+                {
+
+                    if ((responseData.substring(0, 14)) == "<!--SHArdow-->")
+                    {
+                        ((document.getElementsByTagName("html"))[0]).innerHTML = responseData;
+                    }
+                    else if ((responseData.substring(0, 14)) != "<!--SHArdow-->")
+                    {
+                        document.getElementById("tokenInstructions").innerHTML = "invalid token!"
+                    }
+                    else
+                    {
+                        throw "Impossible Condition";
+                    }
+
+                }
+                else if (responseStatus != 200)
+                {
+                    document.getElementById("tokenInstructions").innerHTML = "invalid token!"
+                }
+                else
+                {
+                    throw "Impossible Condition";
+                }
+
+            }
+
+        )
+
+    );
+
+}
+
+forwarder();
+
+
+
