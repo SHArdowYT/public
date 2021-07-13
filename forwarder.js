0fbed3b63af0d297cd83415e271c750a8806f1a4
SHArdow <>
2021-07-13 18:51:11 +1000

    updated webpage

--- forwarder.js
+++ forwarder.js
@@ -3,6 +3,96 @@
 async function forwarder()
 {
 
+    var recent = {}
+
+
+    if ((localStorage.getItem("recent")) == null)
+    {
+        defaultRecent = {"Recent": []};
+        localStorage.setItem("recent", (JSON.stringify(defaultRecent)));
+        recent = (JSON.parse(localStorage.getItem("recent")));
+    }
+    else if ((localStorage.getItem("recent")) != null)
+    {
+        recent = (JSON.parse(localStorage.getItem("recent")));
+
+        var repeats = 0;
+        while ((repeats < ((recent["Recent"]).length)) && (repeats < 4))
+        {
+            document.getElementById("tableRow" + (String(repeats))).innerHTML = (recent["Recent"][repeats]);
+            repeats = repeats + 1;
+        }
+
+    }
+
+
+
+
+
+    document.getElementById("tableRow0").addEventListener
+    (
+
+        "mousedown",
+        (
+
+            async function()
+            {
+                document.getElementById("tokenInput").value = (recent["Recent"][0]);
+            }
+
+        )
+
+    );
+
+    document.getElementById("tableRow1").addEventListener
+    (
+
+        "mousedown",
+        (
+
+            async function()
+            {
+                document.getElementById("tokenInput").value = (recent["Recent"][1]);
+            }
+
+        )
+
+    );
+
+    document.getElementById("tableRow2").addEventListener
+    (
+
+        "mousedown",
+        (
+
+            async function()
+            {
+                document.getElementById("tokenInput").value = (recent["Recent"][2]);
+            }
+
+        )
+
+    );
+
+    document.getElementById("tableRow3").addEventListener
+    (
+
+        "mousedown",
+        (
+
+            async function()
+            {
+                document.getElementById("tokenInput").value = (recent["Recent"][3]);
+            }
+
+        )
+
+    );
+
+
+
+
+
     document.getElementById("tokenSubmit").addEventListener
     (
 
@@ -23,6 +113,9 @@ async function forwarder()
 
                     if ((responseData.substring(0, 14)) == "<!--SHArdow-->")
                     {
+                        console.log(recent);
+                        (recent["Recent"]).unshift(token);
+                        localStorage.setItem("recent", (JSON.stringify(recent)));
                         document.open();
                         document.write(responseData);
                         document.close();
