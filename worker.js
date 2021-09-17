a3260b551e6dc7385b951f0028b00d1b042d38a8
SHArdow <63227636+SHArdowYT@users.noreply.github.com>
2021-09-17 11:36:26 +1000

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,117 @@
+
+
+
+self.addEventListener
+(
+
+    "install",
+    (
+
+        async function(event)
+        {
+
+            await event;
+            var piscesCaches = await caches.open("pisces");
+            return piscesCaches.addAll
+            (
+                [
+                    "/",
+                ]
+            );
+
+
+
+        }
+
+    )
+
+);
+
+
+
+
+
+
+
+
+`
+self.addEventListener
+(
+
+    "fetch",
+    function (event)
+    {
+        event.respondWith
+        (
+            caches.match(event.request)
+            .then
+            (
+                function(response)
+                {
+
+                    if ((!!response) == true)
+                    {
+                        return response
+                    }
+                    else if ((!!response) == false)
+                    {
+                        return fetch(event.request)
+                        .then
+                        (
+                            async function (response)
+                            {
+                                var cache = await caches.open("pisces");
+                                cache.put((event.request), (response.clone()));
+                                return response
+                            }
+                        )
+                    }
+
+
+                }
+            )
+        )
+    }
+
+)
+
+
+
+`
+
+
+
+self.addEventListener
+(
+
+    "fetch",
+    function (event)
+    {
+        event.respondWith
+        (
+
+            caches.open("pisces")
+            .then
+            (
+                function (cache)
+                {
+                    return fetch(event.request)
+                    .then
+                    (
+                        function (response)
+                        {
+                            cache.put((event.request), (response.clone()));
+                            return response;
+
+                        }
+                    )
+                }
+            )
+
+        )
+    }
+
+)
+
+
+
